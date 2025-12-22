6af40a530e2fc395673e8d0f39a2606c593a3153
Your Name <dicksonwachira886@gmail.com>
2025-12-22 18:37:28 +0300

    Implement user update functionality

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,3 +1,7 @@
+import bcrypt from 'bcryptjs';
+import { v2 as cloudinary } from 'cloudinary';
+
+//models
 import User from '../models/user.model.js';
 import Notification from '../models/notification.model.js';
 
@@ -77,6 +81,65 @@ export const getSuggestedUsers = async (req, res) => {
         res.status(200).json(suggestedUsers);
     }catch(error){
         console.log("Error in getSuggestedUsers controller", error.message);
+        res.status(500).json({error: error.message});
+    }
+}
+
+export const updateUser = async (req, res) => {
+    const {fullName, email, username, currentPassword, newPassword, bio, link} = req.body;
+    let { profileImg, coverImg } = req.body;
+
+    const userId = req.user._id;
+
+    try{
+        let user = await User.findById(userId);
+        if(!user) return res.status(404).json({ error: "User not found" });
+
+        if((!newPassword && currentPassword) || (newPassword && !currentPassword)) {
+            return res.status(400).json({ error: "Both current and new passwords are required to change password" });
+        }
+
+        if(currentPassword && newPassword) {
+            const isMatch = await bcrypt.compare(currentPassword, user.password);
+            if(!isMatch) return res.status(400).json({ error: "Current password is incorrect" });
+            if(newPassword.length < 6) {
+                return res.status(400).json({ error: "Password must be at least 6 characters long" });
+            }
+
+            const salt = await bcrypt.genSalt(10);
+            const hashedPassword = await bcrypt.hash(newPassword, salt);
+            user.password = hashedPassword;
+        }
+        if(profileImg) {
+            if(user.profileImg) {
+                await cloudinary.uploader.destroy(user.profileImg.split("/").pop().split(".")[0]);
+            }
+
+            const uploadedResponse = await cloudinary.uploader.upload(profileImg);
+            profileImg = uploadedResponse.secure_url;
+        }
+        if(coverImg) {
+            if(user.coverImg) {
+                await cloudinary.uploader.destroy(user.coverImg.split("/").pop().split(".")[0]);
+            }
+            const uploadedResponse = await cloudinary.uploader.upload(coverImg);
+            coverImg = uploadedResponse.secure_url;
+        }
+
+        user.fullName = fullName || user.fullName;
+        user.email = email || user.email;
+        user.username = username || user.username;
+        user.bio = bio || user.bio;
+        user.link = link || user.link;
+        user.profileImg = profileImg || user.profileImg;
+        user.coverImg = coverImg || user.coverImg;
+
+        user = await user.save();
+        user.password = null; //password should be null in the response
+        res.status(200).json(user);
+
+    } catch(error){
+        console.log("Error in updateUser controller", error.message);
         res.status(500).json({error: error.message});
     }
 }
